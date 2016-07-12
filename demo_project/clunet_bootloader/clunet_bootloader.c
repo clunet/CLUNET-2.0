238f3b2832ddcb9aede411253897321402d4cf30
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2016-07-12 22:40:25 +0400

    Update clunet_bootloader.c

--- demo_project/clunet_bootloader/clunet_bootloader.c
+++ demo_project/clunet_bootloader/clunet_bootloader.c
@@ -14,12 +14,11 @@
 #define COMMAND_FIRMWARE_UPDATE_WRITTEN 4
 #define COMMAND_FIRMWARE_UPDATE_DONE 5
 
-#define APP_END (FLASHEND - (BOOTSIZE * 2))
+//#define APP_END (FLASHEND - (BOOTSIZE * 2))
 #define COMMAND buffer[CLUNET_OFFSET_COMMAND]
 #define SUB_COMMAND buffer[CLUNET_OFFSET_DATA]
 
 #define PAUSE(t) {CLUNET_TIMER_REG = 0; while(CLUNET_TIMER_REG < (t*CLUNET_T));}
-#define SEND_BIT(t) {CLUNET_SEND_1; PAUSE(t); CLUNET_SEND_0; PAUSE(1);}
 
 #define WAIT_INTERFRAME(t) { CLUNET_TIMER_REG = 0; while(CLUNET_TIMER_REG < (t*CLUNET_T)) if(CLUNET_READING) CLUNET_TIMER_REG = 0; }
 
@@ -29,13 +28,13 @@
 #define MY_SPM_PAGESIZE SPM_PAGESIZE
 #endif
 
-volatile char update = 0;
+volatile uint8_t update = 0;
 
-char buffer[MY_SPM_PAGESIZE+0x10];
+static uint8_t buffer[MY_SPM_PAGESIZE+0x10];
 static void (*jump_to_app)(void) = 0x0000;
 
-char
-check_crc(const char* data, const uint8_t size)
+static uint8_t
+check_crc(const uint8_t* data, const uint8_t size)
 {
       uint8_t crc = 0;
       uint8_t i, j;
@@ -55,15 +54,15 @@ check_crc(const char* data, const uint8_t size)
       return crc;
 }
 
-void
-send(const char* data, const uint8_t size)
+static void
+send(const uint8_t* data, const uint8_t size)
 {
 
 	uint8_t numBits = 4;
 	uint8_t bitIndex = 0;
 	uint8_t byteIndex = 0;
 
-	char crc = check_crc(data, size);
+	uint8_t crc = check_crc(data, size);
 
 	WAIT_INTERFRAME(7);
 	
@@ -124,14 +123,15 @@ wait_for_impulse()
 {
 	uint8_t ticks = 0;
 	uint8_t time = 0;
-	uint8_t i;
 
 	CLUNET_TIMER_REG = 0;
 	CLUNET_TIMER_OVERFLOW_CLEAR;
 
+	uint8_t i;
 
 	for (i = 0; i < 2; i++)
 	{
+
 		while(CLUNET_READING != CLUNET_READING)
 			if (CLUNET_TIMER_OVERFLOW)
 			{
@@ -143,24 +143,9 @@ wait_for_impulse()
 
 		ticks = CLUNET_TIMER_REG - ticks;
 	
-//		time = 0;
+		time = 254;		// на 2-м проходе переполнение не должно быть более 1 раза, иначе ошибка
+
 	}
-
-/*	
-
-
-	while(CLUNET_READING != CLUNET_READING)
-		if (CLUNET_TIMER_OVERFLOW)
-		{
-			// Ошибка: слишком большой период
-			if (++time > 1)
-				return 0;
-			CLUNET_TIMER_OVERFLOW_CLEAR;
-		}
-
-	ticks = CLUNET_TIMER_REG - ticks;
-	
-*/
 
 	if (ticks < CLUNET_READ1)
 		ticks = 1;
@@ -178,16 +163,13 @@ wait_for_impulse()
 
 	// Возвращаем длину импульса в периодах CLUNET_T, если пришли единички, то старший бит равен 1
 	return (CLUNET_READING) ? ticks : (ticks | 0x80);
+
 }
 
 static uint8_t
 read()
 {
 
-	uint8_t bitIndex = 0;
-	uint8_t byteIndex = 0;
-	uint8_t bitStuff = 0;
-	
 	WAIT_INTERFRAME(7);
 
 	uint8_t data = wait_for_impulse();
@@ -195,39 +177,45 @@ read()
 	// Если биты доминантные и их не менее 4 (стартовый + 3 бита приоритета), то этот пакет нам подходит, начнем прием и разбор
 	if (data >= 0x84)
 	{
+	
+		uint8_t byteIndex = 0;
+		uint8_t bitIndex, bitStuff;
 
-		// Если пришло 5 бит, то последний относится к данным, установим его
-		if (data & 1)
-		{
-			buffer[0] = data;
-			bitIndex = bitStuff = 1;
-		}
-		
-		do
+		buffer[0] = data;
+		bitIndex = bitStuff = data & 1;
+
+		while(1)
 		{
 		
 			data = wait_for_impulse();
 			
 			if (data)
 			{
-				uint8_t bitNum = data & 7;
+				uint8_t bitNum = data & 0x7F;
+				uint8_t optByte = (data & 0x80) ? 0xFF : 0x00;
 				
-				if (data & 0x80)
+				if (optByte)
 					buffer[byteIndex] |= (255 >> bitIndex);
 				else
 					buffer[byteIndex] &= ~(255 >> bitIndex);
 
 				// Обновим битовый индекс с учетом битстаффинга
 				bitIndex += (bitNum - bitStuff);
+
+				// Смотрим надо ли применять битстаффинг
 				bitStuff = (bitNum == 5);
 
 				if (bitIndex & 8)
 				{
+					/* Пакет прочитан полностью, выходим из цикла, проверяем и возвращаем управление */
+					if ((++byteIndex > CLUNET_OFFSET_SIZE) && (byteIndex > buffer[CLUNET_OFFSET_SIZE] + CLUNET_OFFSET_DATA))
+						break;
+
 					/* Если данные прочитаны не полностью и мы не выходим за пределы буфера, то присвоим очередной байт и подготовим битовый индекс */
-					if (++byteIndex < CLUNET_READ_BUFFER_SIZE)
+					else if (byteIndex < CLUNET_READ_BUFFER_SIZE)
 					{
 						bitIndex &= 7;
-						buffer[byteIndex] = (data & 0x80) ? 0xFF : 0x00;
+						buffer[byteIndex] = optByte;
 					}
 
 					/* Иначе ошибка: нехватка приемного буфера -> игнорируем пакет */
@@ -241,10 +229,9 @@ read()
 			else
 				return 0;
 		}
-		while ((byteIndex < CLUNET_OFFSET_DATA) || (byteIndex <= buffer[CLUNET_OFFSET_SIZE] + CLUNET_OFFSET_DATA));
 
 		// Пакет принят
-		if ((!check_crc((char*)buffer, byteIndex)) && (buffer[CLUNET_OFFSET_DST_ADDRESS] == CLUNET_DEVICE_ID) &&
+		if ((!check_crc(buffer, byteIndex)) && (buffer[CLUNET_OFFSET_DST_ADDRESS] == CLUNET_DEVICE_ID) &&
 			(buffer[CLUNET_OFFSET_COMMAND] == CLUNET_COMMAND_BOOT_CONTROL))
 			return buffer[CLUNET_OFFSET_SIZE];
 	
@@ -254,7 +241,7 @@ read()
 }
 
 static void
-write_flash_page(uint32_t address, char* pagebuffer)
+write_flash_page(uint32_t address, uint8_t* pagebuffer)
 {
 	eeprom_busy_wait();
 
@@ -263,19 +250,19 @@ write_flash_page(uint32_t address, char* pagebuffer)
 #endif
 	{
 		boot_page_erase (address);
-		boot_spm_busy_wait ();      // Wait until the memory is erased.
+		boot_spm_busy_wait();		// Wait until the memory is erased.
 	}
 
 	int i;
-	for (i=0; i<MY_SPM_PAGESIZE; i+=2)
+	for (i = 0; i < MY_SPM_PAGESIZE; i += 2)
 	{
 		// Set up little-endian word.
 		uint16_t w = *((uint16_t*)(pagebuffer + i));
 		boot_page_fill (address + i, w);
 	}
 
-	boot_page_write(address);     // Store buffer in flash page.
-	boot_spm_busy_wait();            // Wait until the memory is written.
+	boot_page_write(address);	// Store buffer in flash page.
+	boot_spm_busy_wait();		// Wait until the memory is written.
 
 	boot_rww_enable ();
 }
@@ -283,15 +270,15 @@ write_flash_page(uint32_t address, char* pagebuffer)
 static void
 send_firmware_command(char b)
 {
-	char update_start_command[5] = { CLUNET_DEVICE_ID, CLUNET_BROADCAST_ADDRESS, CLUNET_COMMAND_BOOT_CONTROL, 1, b };
-	send(update_start_command, 5);
+	uint8_t update_start_command[5] = { CLUNET_DEVICE_ID, CLUNET_BROADCAST_ADDRESS, CLUNET_COMMAND_BOOT_CONTROL, 1, b };
+	send(update_start_command, sizeof(update_start_command));
 }
 
 static void
 firmware_update()
 {
-	char update_start_command[7] = {CLUNET_DEVICE_ID,CLUNET_BROADCAST_ADDRESS,CLUNET_COMMAND_BOOT_CONTROL,3,COMMAND_FIRMWARE_UPDATE_READY,(MY_SPM_PAGESIZE & 0xFF),(MY_SPM_PAGESIZE>>8)};
-	send(update_start_command, 7);
+	uint8_t update_start_command[7] = {CLUNET_DEVICE_ID,CLUNET_BROADCAST_ADDRESS,CLUNET_COMMAND_BOOT_CONTROL,3,COMMAND_FIRMWARE_UPDATE_READY,(MY_SPM_PAGESIZE & 0xFF),(MY_SPM_PAGESIZE>>8)};
+	send(update_start_command, sizeof(update_start_command));
 	while(1)
 	{
 		if (read())
@@ -304,7 +291,7 @@ firmware_update()
 			case COMMAND_FIRMWARE_UPDATE_WRITE:
 			{
 				uint16_t address = *((uint32_t*)(buffer + (CLUNET_OFFSET_DATA + 1)));
-				char* pagebuffer = buffer + (CLUNET_OFFSET_DATA + 5);
+				uint8_t* pagebuffer = buffer + (CLUNET_OFFSET_DATA + 5);
 				write_flash_page(address, pagebuffer);
 				send_firmware_command(COMMAND_FIRMWARE_UPDATE_WRITTEN);
 			}
