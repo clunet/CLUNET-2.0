812711dda65265cd883b6cc83f8361500bcc95b8
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2016-07-14 09:58:46 +0400

    Update clunet-demo.c

--- demo_project/clunet-demo.c
+++ demo_project/clunet-demo.c
@@ -16,12 +16,12 @@ int main (void)
 	clunet_set_on_data_received(data_received);
 	sei();
 
-	char buffer[1];
+	uint8_t buffer[1];
 	buffer[0] = 1;
 	clunet_send(CLUNET_BROADCAST_ADDRESS, CLUNET_PRIORITY_MESSAGE, CLUNET_COMMAND_DEVICE_POWER_INFO, buffer, sizeof(buffer));
 	
 	while (1)
-	{	
+	{
 	}
 	return 0;
 }
