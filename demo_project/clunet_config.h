e5c6b3a3ef436eff032510210a0f66b52f5754f2
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2016-07-14 18:42:53 +0400

    Update clunet_config.h

--- demo_project/clunet_config.h
+++ demo_project/clunet_config.h
@@ -1,12 +1,7 @@
-/* Name: clunet_config.h
- * Project: CLUNET network driver
- * Author: Alexey Avdyukhin
- * Creation Date: 2012-11-08
- * License: DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
- */
- 
- #ifndef __clunet_config_h_included__
+#ifndef __clunet_config_h_included__
 #define __clunet_config_h_included__
+
+/* CONFIGURE YOUR DEVICE HERE */
 
 /* Device address (0-254) */
 #define CLUNET_DEVICE_ID 99
@@ -32,28 +27,40 @@
 /* Timer prescaler */
 #define CLUNET_TIMER_PRESCALER 64
 
-/* Custom T (T >= 8 && T <= 24). 
- T is frame unit size in timer ticks. Lower - faster, highter - more stable
- If not defined T will be calculated as ~64us based on CLUNET_TIMER_PRESCALER value
+/*
+	Custom T (T >= 8 && T <= 24).
+	T is frame unit size in timer ticks. Lower - faster, highter - more stable.
+	If not defined T will be calculated as ~64us based on CLUNET_TIMER_PRESCALER value.
 */
- //#define CLUNET_T 8
+//#define CLUNET_T 8
 
-/* Timer initialization */
-#define CLUNET_TIMER_INIT {unset_bit4(TCCR2, WGM21, WGM20, COM21, COM20); /* Timer2, normal mode */ \
-	set_bit(TCCR2, CS22); unset_bit2(TCCR2, CS21, CS20); /* 64x prescaler */ }
+/* Timer initialization in NORMAL MODE */
+#define CLUNET_TIMER_INIT \
+			{ \
+				unset_bit4(TCCR2, WGM21, WGM20, COM21, COM20); \
+				set_bit(TCCR2, CS22); \
+				unset_bit2(TCCR2, CS21, CS20); \
+			}
 
-/* Timer registers */
+/* 8-bit Timer/Counter definitions */
+
+// Main Register
 #define CLUNET_TIMER_REG TCNT2
+// Output Compare Register
 #define CLUNET_TIMER_REG_OCR OCR2
+// Overflow Condition
 #define CLUNET_TIMER_OVERFLOW (TIFR & (1 << TOV2))
+// Reset Overflow Flag Command
 #define CLUNET_TIMER_OVERFLOW_CLEAR set_bit(TIFR, TOV2)
 
+/* End of Timer/Counter definitions */
+
 /* How to enable and disable timer interrupts */
-#define CLUNET_ENABLE_TIMER_COMP {set_bit(TIFR, OCF2);set_bit(TIMSK, OCIE2);}
+#define CLUNET_ENABLE_TIMER_COMP { set_bit(TIFR, OCF2); set_bit(TIMSK, OCIE2); }
 #define CLUNET_DISABLE_TIMER_COMP unset_bit(TIMSK, OCIE2)
 
 /* How to init and enable external interrupt (read pin) */
-#define CLUNET_INIT_INT {set_bit(MCUCR,ISC00);unset_bit(MCUCR,ISC01); set_bit(GICR, INT0);}
+#define CLUNET_INIT_INT { set_bit(MCUCR,ISC00); unset_bit(MCUCR,ISC01); set_bit(GICR, INT0); }
 
 /* Interrupt vectors */
 #define CLUNET_TIMER_COMP_VECTOR TIMER2_COMP_vect
