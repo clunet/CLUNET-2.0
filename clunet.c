ad50301bbed6b3bba373ad44b2c5441594c099dd
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2016-07-15 16:18:29 +0400

    Update clunet.c

--- clunet.c
+++ clunet.c
@@ -57,7 +57,8 @@ const static char devName[] = CLUNET_DEVICE_NAME; // Имя устройства
 static char
 check_crc(const char* data, const uint8_t size)
 {
-      uint8_t crc = 0, a = 0, b;
+      char crc = 0;
+      uint8_t a = 0, b;
       do
       {
             char inbyte = data[a];
